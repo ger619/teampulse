8d5af7665f6dcd90792dfdd73eda4800b945df78
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-11-28 23:16:37 +0300

    Add the Redux store to the whole app layer

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import './App.css'
+import store from "./redux/store";
+import { Provider } from "react-redux";
+import './App.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
